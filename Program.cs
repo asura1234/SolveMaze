62a4b4a2aed6041a6b4bf2a49e92f853e5bf007d
asura1234 <yliu8949@gmail.com>
2018-01-29 19:39:55 -0500

    Initial check-in of module SolveMaze

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolveMaze
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
